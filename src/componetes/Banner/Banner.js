3a95934215a068b18d9c296270b075b44b757445
Gabriel de Carvalho <gabrielcf4@gmail.com>
2025-08-25 17:05:02 -0300

    first commit

---
+++ src/componetes/Banner/Banner.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react/jsx-runtime'
+import './Banner.css'
+function Banner() {
+// JSX
+    return (
+    <Fragment>
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="banner principal"/>
+        </header>
+    </Fragment>
+)
+}
+
+export default Banner
